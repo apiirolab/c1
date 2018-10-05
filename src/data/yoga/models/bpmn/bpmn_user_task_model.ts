51f9cb608f86049158f1bd0a0297cabc03ca2c38
Tomas Trescak <tomi.trescak@gmail.com>
2018-10-05 11:41:13 +1000

    Improved log management

--- src/data/yoga/models/bpmn/bpmn_user_task_model.ts
+++ src/data/yoga/models/bpmn/bpmn_user_task_model.ts
@@ -41,7 +41,7 @@ export class UserTask extends Task {
               id: state.id
             }
           },
-          status: 'Waiting'
+          status: 'Started'
         }
       });
       result.active.push(taskInstanceDAO.id);
